4aaa75d6b1fac46babd6b826098e88a0f0737452
wairimu.Kanene <mercykanene254@gmail.com>
2022-05-02 15:21:48 +0300

    commit4-added parent configuration class

---
+++ app/config.py
@@ -0,0 +1,21 @@
+class Config:
+  '''
+  General configuration parent class
+  '''
+  pass
+
+
+class ProdConfig(Config):
+  '''
+  Production configuration child class
+
+  Args:
+  Config: The parent configuration class with General configuration settings
+  '''
+  pass
+
+
+
+  
+
+
