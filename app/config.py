7f364842a9fc19e6e8453abfd5bdc6c73b381838
wairimu.Kanene <mercykanene254@gmail.com>
2022-05-02 15:59:38 +0300

    commit8-added the url links on the config class

--- app/config.py
+++ app/config.py
@@ -1,8 +1,12 @@
+import os
 class Config:
     '''
     General configuration parent class
     '''
-    pass
+    NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 
