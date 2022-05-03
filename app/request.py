824c6edfed79ae91ad4ab4dbc0e227fcc652c585
wairimu.Kanene <mercykanene254@gmail.com>
2022-05-03 10:41:47 +0300

    commit15-imported flask application instance with helps us connect with the api url

--- app/request.py
+++ app/request.py
@@ -1,4 +1,10 @@
 from app import app
+import urllib.request,json
+from app.models import Source,Articles
 
 #Getting api key
-api_key = app.config['NEW_API_KEY']
+api_key = app.config['NEWS_API_KEY']
+
+#Getting the news base url
+base_url = app.config['NEWS_SOURCES_BASE_URL']
+articles_url = app.config['ARTICLES_BASE_URL']
