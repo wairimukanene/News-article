b4cc49d1a548492eed182d2c6f91b80ab7a57c9e
wairimu.Kanene <mercykanene254@gmail.com>
2022-05-03 16:37:52 +0300

    commit24-created navbar.html installed bootstrap and extended it base.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
 #Intializing application
 app = Flask(__name__,instance_relative_config = True)
@@ -7,5 +8,10 @@ app = Flask(__name__,instance_relative_config = True)
 # Setting up configuration
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
+
+
 
 from app import views
