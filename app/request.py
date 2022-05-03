e7537804d5ad56c7e4f7b1786560e5c5a76c9ab3
wairimu.Kanene <mercykanene254@gmail.com>
2022-05-03 11:02:02 +0300

    commit16-function that gets the json response to our url request

--- app/request.py
+++ app/request.py
@@ -24,4 +24,19 @@ def get_sources(category):
   '''
   Function that gets the json response to our url request
   '''
+  get_sources_url = base_url.format(category,api_key)
+
+
+  with urllib.request.urlopen(get_sources_url) as url:
+    get_sources_data = url.read()
+    get_sources_response = json.loads(get_sources_data)
+
+    source_results = None
+
+    if get_sources_response['sources']:
+      sources_results_list = get_sources_response['sources']
+      sources_results = process_sources(sources_results_list)
+
+      return sources_results
+
 
