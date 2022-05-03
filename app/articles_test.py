0a272db307fe5c695b8be60bf91686c5172879f6
wairimu.Kanene <mercykanene254@gmail.com>
2022-05-03 09:17:41 +0300

    commit10-article_test py added setup function

---
+++ app/articles_test.py
@@ -0,0 +1,13 @@
+import unittest
+from app.models import Articles
+
+
+class ArticlesTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Movie class
+    '''
+
+    def setup(self):
+      '''
+      Set up method that will run before every Test
+      '''
