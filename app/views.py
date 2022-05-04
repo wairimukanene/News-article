5b451212b9ed31f11742bc7df0ade78c045e7daf
wairimu.Kanene <mercykanene254@gmail.com>
2022-05-04 10:26:38 +0300

    commit29-indentation spacing

--- app/views.py
+++ app/views.py
@@ -5,28 +5,22 @@ from .request import get_sources,get_article
 # Views
 @app.route('/')
 def index():
-  
   '''
   View root page function that returns the index page and its data
   '''
 
   #Getting business related sources
-business_sources = get_sources('business')
+  business_sources = get_sources('business')
 
 #Getting technology related sources
-tech_sources = get_sources('technology')
+  tech_sources = get_sources('technology')
 
 #Getting sports related sources
-sports_sources = get_sources('sports')
+  sports_sources = get_sources('sports')
 
-print(tech_sources)
-title = 'The Daily News'
-
+  print(tech_sources)
+  title = 'The Daily News'
   return render_template('index.html', title = title, technology = tech_sources, business = business_sources, sports = sports_sources)
-
-
-
-
 
 @app.route('/source/<id>')
 def articles(id):
